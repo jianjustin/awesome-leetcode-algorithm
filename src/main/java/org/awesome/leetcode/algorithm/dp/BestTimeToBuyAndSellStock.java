b67f63f54782e08851484c899c78b804106da010
eatMelon-Masses <z11764404@gmail.com>
2019-03-30 22:39:49 +0800

    feat: 买卖股票最佳时机题解2，优化速度

--- src/main/java/org/awesome/leetcode/algorithm/dp/BestTimeToBuyAndSellStock.java
+++ src/main/java/org/awesome/leetcode/algorithm/dp/BestTimeToBuyAndSellStock.java
@@ -29,5 +29,18 @@ public class BestTimeToBuyAndSellStock {
         
         return resultMoney;
     }
-
+    public int maxProfit2(int[] prices) {
+        int buymin=Integer.MAX_VALUE;
+        int sum=0;
+        if(prices.length<2) 
+            return 0;
+        else
+            buymin=prices[0];
+        for(int i=1;i<=prices.length-1;i++){           
+            sum=(prices[i]-buymin)>sum?(prices[i]-buymin):sum;
+            if(prices[i]<buymin) buymin=prices[i]; 
+        }
+        
+        return sum;
+    }
 }
