4525440e8138b659be877ffb7057890784f6c21a
yiezi <11764404@qq.com>
2019-04-09 23:05:56 +0800

    fix: 添加函数注解

--- src/main/java/org/awesome/leetcode/algorithm/greedy/GasStation.java
+++ src/main/java/org/awesome/leetcode/algorithm/greedy/GasStation.java
@@ -2,6 +2,8 @@ package org.awesome.leetcode.algorithm.greedy;
 
 
 /**
+ * @author eatMelon-Masses
+ * @date 2019年4月9日
  * No.134加油站
  * 在一条环路上有 N 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
  *
@@ -25,6 +27,12 @@ package org.awesome.leetcode.algorithm.greedy;
  *
  */
 public class GasStation {
+    /**
+     *
+     * @param gas 每个站可加油量
+     * @param cost 到下一站消耗的油量
+     * @return 如果能跑完全程返回数组下标,否则返回-1
+     */
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int[] remains = remainingGas(gas, cost);
         int result = -1;
@@ -36,6 +44,12 @@ public class GasStation {
         return result;
     }
 
+    /**
+     * 根据gas数组与cost数组的差值计算出每站剩余油量
+     * @param gas 每个站可加油量
+     * @param cost 到下一站消耗的油量
+     * @return 每站剩余油量
+     */
     public int[] remainingGas(int[] gas, int[] cost) {
         int[] remains = new int[gas.length];
         for (int i = 0; i < gas.length; i++) {
@@ -44,6 +58,12 @@ public class GasStation {
         return remains;
     }
 
+    /**
+     * 是否能够跑完全程
+     * @param remains 每站剩余油量
+     * @param startIndex 从下标为几的站台出发
+     * @return 是-true 否-false
+     */
     public boolean canRun(int[] remains,int startIndex) {
         int currGas = 0;
         for (int i = 1; i <= remains.length; i++) {
