eb91a7c0e9982c1a3dd2089c51995ac13dba6e54
jian <janine.jian.chen@gmail.com>
2020-03-08 14:36:49 +0800

    面试题22. 链表中倒数第k个节点

---
+++ src/main/java/org/awesome/leetcode/algorithm/arrays/LianBiaoZhongDaoShuDiKgeJieDianLcof.java
@@ -0,0 +1,26 @@
+package org.awesome.leetcode.algorithm.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LianBiaoZhongDaoShuDiKgeJieDianLcof {
+
+	public ListNode getKthFromEnd(ListNode head, int k) {
+		List<ListNode> nodes = new ArrayList<>();
+		
+		while (null != head) {
+			nodes.add(head);
+			head = head.next;
+		}
+		
+		if(nodes.size() < k)return null;
+		return nodes.get(nodes.size()-k);
+    }
+	
+}
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+}
