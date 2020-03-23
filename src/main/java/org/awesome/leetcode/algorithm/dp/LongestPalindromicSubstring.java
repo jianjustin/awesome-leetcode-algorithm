40ccd1093e1471187d5b7b02231420a22e46b292
jian <janine.jian.chen@gmail.com>
2020-03-23 17:52:44 +0800

    Manacher算法

--- src/main/java/org/awesome/leetcode/algorithm/dp/LongestPalindromicSubstring.java
+++ src/main/java/org/awesome/leetcode/algorithm/dp/LongestPalindromicSubstring.java
@@ -8,27 +8,62 @@ package org.awesome.leetcode.algorithm.dp;
 public class LongestPalindromicSubstring {
 	
 	public String longestPalindrome(String s) {
-        if(null == s || s.length() == 0 || s.length() == 1)return s;
-        
-        int len = s.length(), start = 0, maxlen = 1;
-        boolean[][] dp = new boolean[len][len];
-        
-        for (int i = 0; i < len; i++) dp[i][i] = true;// init
-        
-        for (int i = 1; i < len; i++) {//end index
-        	for (int j = 0; j < i; j++) {//start index
-				if(s.charAt(i) != s.charAt(j)) continue;
-				dp[j][i] = i-j<3?true:dp[j+1][i-1];
-				if (dp[j][i]) {
-                    if (i - j + 1 > maxlen) {
-                        maxlen = i - j + 1;
-                        start = j;
-                    }
-                }
-			}
-		}
-        return s.substring(start, start+maxlen);
+		if(null == s || s.length() == 0 || s.length() == 1)return s;
+
+        String str = addBoundaries(s, '#');// 得到预处理字符串
+        int sLen = 2 * s.length() + 1;
+        int[] p = new int[sLen];
+
+        int maxRight = 0, center = 0, maxLen = 1, start = 0;
+
+        for (int i = 0; i < sLen; i++) {
+            if (i < maxRight) {
+                int mirror = 2 * center - i;//获取镜像位置，减少查找的次数
+                p[i] = Math.min(maxRight - i, p[mirror]);
+            }
+
+            int left = i - (1 + p[i]), right = i + (1 + p[i]);
+
+            while (left >= 0 && right < sLen && str.charAt(left) == str.charAt(right)) {
+                p[i]++;
+                left--;
+                right++;
+
+            }
+         
+            if (i + p[i] > maxRight) {
+                maxRight = i + p[i];
+                center = i;
+            }
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                start = (i - maxLen) / 2;
+            }
+        }
+        return s.substring(start, start + maxLen);
     }
+
+
+    /**
+     * 创建预处理字符串
+     */
+    private String addBoundaries(String s, char divide) {
+        int len = s.length();
+        if (len == 0) {
+            return "";
+        }
+        if (s.indexOf(divide) != -1) {
+            throw new IllegalArgumentException("参数错误，您传递的分割字符，在输入字符串中存在！");
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            stringBuilder.append(divide);
+            stringBuilder.append(s.charAt(i));
+        }
+        stringBuilder.append(divide);
+        return stringBuilder.toString();
+    }
+    
 	
 	public static void main(String[] args) {
 		String string = "babad";
